a186d2e472e5b791bb1c1ee681c111fe5e69d180
DanielHall-Eardley <350chevy8@gmail.com>
2021-10-27 20:05:25 -0400

    test

---
+++ structure/base.js
@@ -0,0 +1,19 @@
+const dirStructure = [
+  {
+    name: 'pages',
+    folders: [
+      {
+        name: 'home',
+        files: ['home.ejs', 'home.js', 'home.css']
+      }
+    ]
+  },
+  'build',
+  {
+    name: 'ssrRoutes',
+    files: ['routes.js']
+  },
+  'routes',
+  'controllers',
+  'util'
+]
